bc2b1f6cbfa45fbbb7bb3a992af8adeb85641896
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 00:07:31 +0530

    to test the cicd server

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <h1>My react App</h1>
         <p>Hii this is a simple website to deploy on AWS</p>
         <p>This is a new line to test</p>
+        <p>Adding a line from local</p>
       </div>
     </>
   )
