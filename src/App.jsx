fd6ae2c7afdeb3d2abdcfdfaeea5326aafe4f5a2
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 00:34:23 +0530

    removed lines

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ function App() {
       <div>
         <h1>My react App</h1>
         <p>Hii this is a simple website to deploy on AWS</p>
-        <p>Final test to deploy on AWS</p>
-        <p>Final Final test to deploy on AWS</p>
-        <p>Final Final Final test to deploy on AWS</p>
       </div>
     </>
   )
