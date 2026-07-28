d6bff81f0c60dcf66f6b32293b217dedf2dfe927
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 00:16:53 +0530

    Final test

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ function App() {
       <div>
         <h1>My react App</h1>
         <p>Hii this is a simple website to deploy on AWS</p>
-        <p>This is a new line to test</p>
-        <p>Adding a line from local</p>
-        <p>Adding a line from local</p>
+        <p>Final test to deploy on AWS</p>
       </div>
     </>
   )
