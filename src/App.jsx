bf3606ce7cc572db38fbe5bb924cdc2a239279de
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 00:03:06 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>My react App</h1>
+        <p>Hii this is a simple website to deploy on AWS</p>
+        <p>This is a new line to test</p>
+      </div>
+    </>
+  )
+}
+
+export default App
