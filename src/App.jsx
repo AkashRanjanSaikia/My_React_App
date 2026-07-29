b79699cdd94ec14076ea0364a618614d819e4473
Akash Saikia <arsaikia26@gmail.com>
2026-07-29 12:06:45 +0530

    to test the config file

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
         <h1>My react App</h1>
         <h1>Good Morning</h1>
         <h2>From Akash Ranjan Saikia</h2>
+        <h2>Line to test the flexible config file</h2>
       </div>
     </>
   )
